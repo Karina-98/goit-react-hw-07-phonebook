9c6f706ffa0e31b1e1fab718a05a492aa9c11c35
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-07-17 20:39:11 +0200

    +

--- src/service/getApi.js
+++ src/service/getApi.js
@@ -1,5 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://64b5771bf3dbab5a95c75bd4.mockapi.io'
 
 export const getContacts = createAsyncThunk('contacts/fetchAll', ()=>{
+try{
+    const res = axios.get('/contacts')
+// return res.data
+return console.log(thunkAPI)
+} catch(e){
 
+}
 })
